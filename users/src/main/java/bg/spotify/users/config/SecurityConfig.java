61ab651c41c35f6410ec6a71d2c3660b02299f0a
Kaloyan Ribarov <108590701+TheFisher1@users.noreply.github.com>
2025-06-24 23:38:39 +0300

    remove unused import

--- users/src/main/java/bg/spotify/users/config/SecurityConfig.java
+++ users/src/main/java/bg/spotify/users/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package bg.spotify.users.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
