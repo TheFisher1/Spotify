b34379cd7dc6f06a96acf977095c96dd16f5eaf8
Kaloyan Ribarov <108590701+TheFisher1@users.noreply.github.com>
2025-06-28 12:17:40 +0300

    add some configuration files

---
+++ actions/src/main/java/bg/spotify/actions/ActionsApplication.java
@@ -0,0 +1,17 @@
+package bg.spotify.actions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication(scanBasePackages = { "bg.spotify.actions", "bg.spotify.artist", "bg.spotify.users", "fmi.spotify.media" })
+@EntityScan(basePackages = { "bg.spotify.actions.model", "bg.spotify.artist.model", "bg.spotify.users.model", "fmi.spotify.media.model" })
+@EnableJpaRepositories(basePackages = { "bg.spotify.actions.repository", "bg.spotify.artist.repository", "bg.spotify.users.repository", "fmi.spotify.media.repository" })
+public class ActionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActionsApplication.class, args);
+    }
+
+}
--- media/src/main/java/fmi/spotify/media/MediaApplication.java
+++ media/src/main/java/fmi/spotify/media/MediaApplication.java
@@ -3,7 +3,7 @@ package fmi.spotify.media;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = { "fmi.spotify.media", "bg.spotify.artist", "bg.spotify.recommendations" })
 public class MediaApplication {
 
 	public static void main(String[] args) {
