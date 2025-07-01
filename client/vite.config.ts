5309330fe4f8a02f1519d94f6115624db7b96524
Kaloyan Ribarov <kaloyanribarov@Kaloyans-MacBook-Air.local>
2025-07-01 13:34:19 +0300

    small refactoring and added trust of self signed ssl certificates

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,42 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import { devConfig } from './src/config';
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/auth': {
+        target: devConfig.baseApiUrl,
+        changeOrigin: true,
+        secure: false,
+      },
+      '/media': {
+        target: devConfig.baseApiUrl,
+        changeOrigin: true,
+        secure: false,
+      },
+      '/artists': {
+        target: devConfig.baseApiUrl,
+        changeOrigin: true,
+        secure: false,
+      },
+      '/likes': {
+        target: devConfig.baseApiUrl,
+        changeOrigin: true,
+        secure: false,
+      },
+      '/follows': {
+        target: devConfig.baseApiUrl,
+        changeOrigin: true,
+        secure: false,
+      },
+      '/recommendations': {
+        target: devConfig.baseApiUrl,
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
